2f8730b1a765e6536ab23f07b0b6b5d476336344
Vito <cornacchia.vi@gmail.com>
2026-04-13 15:46:16 +0200

    fix: set router basename for GitHub Pages subpath

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <AuthProvider>
           <DataProvider>
             <Routes>
